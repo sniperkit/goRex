b38da874afdbe43d18be6a707a840667f0d2b136
amller <arthurmueller95@gmail.com>
2017-04-13 12:07:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,102 @@
+# goRex [![GoDoc](https://godoc.org/github.com/amller/goRex?status.svg)](https://godoc.org/github.com/amller/goRex)
+GoRex is a tool which allows to convert textual data into JSON format.  
+Note that goRex was build with the idea of converting logs.
+
+The file containing the regular expression is set with the flag `regexp`. The default value is ".regexp".  
+
+The data is read from os.Stdin and written to os.Stdout.  
+
+The conversion is based on a regular expression as following:  
+Every line will be evaluated for itself, creating a new JSON object.  
+GoRex evaluates then the regular expression and checks for the capture groups.  
+The JSON has one field for every caputure group and the matches on the textual data
+will be assigned to their corresponding JSON fields.  
+
+For more information about capture groups read https://golang.org/pkg/regexp/syntax  
+
+### Example
+.regexp:  
+```
+\[(?P<Tool>[a-zA-Z0-9]+)\]\[(?P<Date>[0-3][0-9]\.[01][0-9]\.[0-9][0-9]:[0-2][0-9]:[0-6][0-9])\] (?P<MessageType>[a-zA-Z]*): (?P<Message>.*)
+```
+
+input:
+```
+[Tool1][07.04.12:12:25] Warning: A warning message!
+[Tool2][07.04.12:12:30] Error: An error message!
+[Tool3][07.04.12:13:01] Info: An info message!
+[Tool2][07.04.12:16:30] Error: An error message!
+[Tool2][07.04.12:16:30] Warning: A warning message!
+[Tool2][07.04.12:16:30] Warning: A warning message!
+[Tool2][07.04.12:16:30] Warning: A warning message!
+[Tool3][07.04.12:16:44] Info: An info message!
+[Tool3][07.04.12:18:30] Update: An update message!
+[Tool3][07.04.12:18:34] Warning: A warning message!
+[Tool1][07.04.12:18:44] Info: An info message!
+```
+
+output:
+```json
+[
+	{
+		"Date": "07.04.12:12:25",
+		"Message": "A warning message!",
+		"MessageType": "Warning",
+		"Tool": "Tool1"
+	},
+	{
+		"Date": "07.04.12:12:30",
+		"Message": "An error message!",
+		"MessageType": "Error",
+		"Tool": "Tool2"
+	},
+	{
+		"Date": "07.04.12:13:01",
+		"Message": "An info message!",
+		"MessageType": "Info",
+		"Tool": "Tool3"
+	},
+	{
+		"Date": "07.04.12:16:30",
+		"Message": "An error message!",
+		"MessageType": "Error",
+		"Tool": "Tool2"
+	},
+	{
+		"Date": "07.04.12:16:30",
+		"Message": "A warning message!",
+		"MessageType": "Warning",
+		"Tool": "Tool2"
+	},
+	{
+		"Date": "07.04.12:16:30",
+		"Message": "A warning message!",
+		"MessageType": "Warning",
+		"Tool": "Tool2"
+	},
+	{
+		"Date": "07.04.12:16:30",
+		"Message": "A warning message!",
+		"MessageType": "Warning",
+		"Tool": "Tool2"
+	},
+	{
+		"Date": "07.04.12:16:44",
+		"Message": "An info message!",
+		"MessageType": "Info",
+		"Tool": "Tool3"
+	},
+	{
+		"Date": "07.04.12:18:30",
+		"Message": "An update message!",
+		"MessageType": "Update",
+		"Tool": "Tool3"
+	},
+	{
+		"Date": "07.04.12:18:34",
+		"Message": "A warning message!",
+		"MessageType": "Warning",
+		"Tool": "Tool3"
+	}
+]
+```
---
+++ goRex.go
@@ -0,0 +1,111 @@
+// Command goRex provides functionality to break a log down into a JSON structured data format,
+// which is described by regular expressions.
+// The structure of the JSON is determined by the capture groups contained in the regular expression.
+// Input will be read from os.Stdin.
+// Output will be written to os.Stdout.
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"io"
+	"io/ioutil"
+	"log"
+	"os"
+	"regexp"
+	"runtime"
+	"strings"
+)
+
+func main() {
+	regexpFlag := flag.String("regexp", ".regexp", "File containing the regular expression")
+	flag.Parse()
+
+	regexpFile, err := os.Open(*regexpFlag)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer regexpFile.Close()
+	regexpr, err := GetRegexp(regexpFile)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = ConvertLog(os.Stdout, os.Stdin, regexpr)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+// GetRegexp reads content from r and tries to interpret it as a regular expression and returns it.
+func GetRegexp(r io.Reader) (*regexp.Regexp, error) {
+	in, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+	str := string(in)
+	if strings.HasSuffix(str, "\r\n") {
+		str = string(in[:len(in)-2])
+	} else if strings.HasSuffix(str, "\n") {
+		str = string(in[:len(in)-1])
+	}
+	regexpr, err := regexp.Compile(str)
+	if err != nil {
+		return nil, err
+	}
+	return regexpr, nil
+}
+
+// ConvertLog reads the content from r and splits the content based on the caputure groups of regexp
+// and writes the result into w.
+// Note that every line is interpreted for itself.
+func ConvertLog(w io.Writer, r io.Reader, regexpr *regexp.Regexp) error {
+	dataSets, err := ExtractDataSets(r, regexpr)
+	if err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(dataSets, "", "\t")
+	if err != nil {
+		return err
+	}
+
+	_, err = io.WriteString(w, string(b))
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+// ExtractDataSets reads the content of r and splits the content into maps for every line based on the
+// capute groups of regexpr.
+func ExtractDataSets(r io.Reader, regexpr *regexp.Regexp) ([]map[string]string, error) {
+	b, err := ioutil.ReadAll(r)
+	if err != nil {
+		return nil, err
+	}
+
+	var inputLines []string
+	// This is not guarenteed to cut of proper newlines.
+	// If we run on a proper OS and got Wintrash logs, we might end up with \r
+	// at the end of the lines.
+	if strings.ToLower(runtime.GOOS) != "windows" {
+		inputLines = strings.Split(string(b), "\n")
+	} else {
+		inputLines = strings.Split(string(b), "\r\n")
+	}
+	dataSets := make([]map[string]string, len(inputLines)-1)
+	for i, line := range inputLines {
+		if line != "" {
+			dataSet := make(map[string]string)
+			matches := regexpr.FindStringSubmatch(line)
+			for j, name := range regexpr.SubexpNames() {
+				if name != "" {
+					dataSet[name] = matches[j]
+				}
+			}
+			dataSets[i] = dataSet
+		}
+	}
+	return dataSets, nil
+}
